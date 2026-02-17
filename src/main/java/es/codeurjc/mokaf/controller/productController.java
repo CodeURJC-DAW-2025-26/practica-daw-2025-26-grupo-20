2602a837700f86b9bf46a2888d46bcc326146be3
ElineeF <en.freites.2022@alumnos.urjc.es>
2026-02-17 10:03:03 +0100

    Dynamic url in controller and fixing var in mustache

--- src/main/java/es/codeurjc/mokaf/controller/productController.java
+++ src/main/java/es/codeurjc/mokaf/controller/productController.java
@@ -4,18 +4,28 @@ import es.codeurjc.mokaf.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class productController {
 
-  @GetMapping("/product")
-    public String product(Model model) {
-        System.out.println(">>> ENTRANDO EN ProductController /product");
+  @GetMapping("/product/{id}")
+    public String product(@PathVariable Long id, Model model) {
+        System.out.println(">>> ENTRANDO EN ProductController /product/" + id);
+        
+        Product product = new Product(id,
+            "Expreso", 
+            "black cofee strong and aromatic.", 
+            "2.50€",
+            "/images/MenuImages/Hot/Expreso.png", 
+            "Hot"
+        );
+        
         model.addAttribute("title", "Producto · MokaF");
-        model.addAttribute("product", new Product("Expreso","Café negro fuerte y aromático.","2.50€",
-                "/images/MenuImages/Hot/Expreso.png","Hot"));
+        model.addAttribute("product", product);
         model.addAttribute("currentPage", "product");
+        
         return "product";
-}
+    }
 
 }
