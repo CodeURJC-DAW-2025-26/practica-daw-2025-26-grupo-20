a257735d402c3a31442fe132c17b978896f5f289
Blazk0o <guillermo11102001@gmail.com>
2026-02-20 20:30:54 +0100

    user service and user repository with srcf

--- src/main/java/es/codeurjc/mokaf/repository/UserRepository.java
+++ src/main/java/es/codeurjc/mokaf/repository/UserRepository.java
@@ -1,6 +1,14 @@
 package es.codeurjc.mokaf.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import es.codeurjc.mokaf.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
